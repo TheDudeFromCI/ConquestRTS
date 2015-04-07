3d828e1dc2a3b11d99d40dc77fada1fca74c5ffe
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-07 15:53:07 -0700

    Moved rendering system to WraithEngine.

--- ConquestRTS/src/me/ci/World/Camera.java
+++
@@ -1,6 +0,0 @@
-package me.ci.World;
-
-public class Camera{
-	public int x, z;
-	public int scale = 16;
-}
--- ConquestRTS/src/me/ci/World/TileMaterial.java
+++
@@ -1,8 +0,0 @@
-package me.ci.World;
-
-import java.awt.image.BufferedImage;
-
-public interface TileMaterial{
-	public BufferedImage getImage();
-	public boolean isAnimated();
-}
