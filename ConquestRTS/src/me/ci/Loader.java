c2ff48454a02ea1a8e5a5b420ae2169f616b3bde
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-08 00:52:19 -0700

    Preformed several tests, and bug fixes.

--- ConquestRTS/src/me/ci/Loader.java
+++ ConquestRTS/src/me/ci/Loader.java
@@ -1,12 +1,23 @@
 package me.ci;
 
+import java.io.File;
+import javax.imageio.ImageIO;
+import wraith.library.WorldManagement.TileGrid.Chipset;
 import wraith.library.WorldManagement.TileGrid.Map;
 
 public class Loader{
 	public static void main(String[] args){
 		GameWindow window = new GameWindow();
-		WorldPop test = new WorldPop();
-		Map map = new Map(20, 2, 15, test);
+		Chipset chipset = null;
+		try{
+			chipset=new Chipset(ImageIO.read(new File("C:/Users/Phealoon/Desktop/chipset.png")), 16);
+			chipset.generateTileMaterials();
+		}catch(Exception exception){
+			exception.printStackTrace();
+			System.exit(1);
+		}
+		WorldPop test = new WorldPop(chipset);
+		Map map = new Map(20, 16, 15, test);
 		map.setCameraScale(40);
 		MapView view = new MapView(map);
 		window.setPanel(view.getPanel());
--- ConquestRTS/src/me/ci/MapView.java
+++ ConquestRTS/src/me/ci/MapView.java
@@ -18,9 +18,26 @@ public class MapView{
 				g.dispose();
 			}
 		};
+		panel.setDoubleBuffered(false);
 		panel.setPreferredSize(new Dimension(map.getCameraScale()*map.getSizeX(), map.getCameraScale()*map.getSizeZ()));
+		panel.setFocusable(true);
 		new Thread(new Runnable(){
-			public void run(){ while(!disposed)panel.paintImmediately(0, 0, panel.getWidth(), panel.getHeight()); }
+			public void run(){
+				int step = 0;
+				long time = System.nanoTime();
+				long temp;
+				while(!disposed){
+					panel.paintImmediately(0, 0, panel.getWidth(), panel.getHeight());
+					step++;
+					if(step==100){
+						step=0;
+						temp=System.nanoTime();
+						double seconds = (temp-time)/100.0/1000000000.0;
+						System.out.println((int)(100/seconds)+" Fps");
+						time=temp;
+					}
+				}
+			}
 		}).start();
 	}
 	public void dispose(){
