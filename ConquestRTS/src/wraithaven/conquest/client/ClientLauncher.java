720836b9afa3bd625a9d29725176d98d8e666530
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-13 03:27:39 -0700

    Client now translates packets properly, and prints message accordingly.

--- ConquestRTS/src/wraithaven/conquest/client/ClientLauncher.java
+++ ConquestRTS/src/wraithaven/conquest/client/ClientLauncher.java
@@ -1,5 +1,9 @@
 package wraithaven.conquest.client;
 
+import wraithaven.conquest.ChatMessagePacket;
+import wraithaven.conquest.KickMessagePacket;
+import wraithaven.conquest.Packet;
+import wraithaven.conquest.PacketType;
 import wraithaven.conquest.HandshakePacket;
 import wraith.library.WindowUtil.ChatColor;
 import wraith.library.Multiplayer.ClientListener;
@@ -14,16 +18,28 @@ public class ClientLauncher{
 		console=new ColorConsole(true);
 		console.setTitle("Wraithaven's Conquest");
 		console.addConsoleListener(new ConsoleListener(){
-			public void onCommandSent(String msg){ client.send("000Player¥"+msg); }
+			public void onCommandSent(String msg){ client.send("000Player¤"+msg); }
 		});
 		client=new Client("localhost", 10050, new ClientListener(){
-			public void recivedInput(String s){ console.println(ChatColor.YELLOW+s); }
+			public void recivedInput(String s){
+				Packet packet = PacketType.create(s);
+				if(packet==null)return;
+				if(packet instanceof KickMessagePacket){
+					console.println(ChatColor.ORANGE+((KickMessagePacket)packet).getMessage());
+					return;
+				}
+				if(packet instanceof ChatMessagePacket){
+					ChatMessagePacket chatPacket = (ChatMessagePacket)packet;
+					console.println(ChatColor.YELLOW+chatPacket.getSender()+": "+chatPacket.getMessage());
+					return;
+				}
+			}
 			public void unknownHost(){ console.println(ChatColor.RED+"Unknown host."); }
 			public void serverClosed(){ console.println(ChatColor.RED+"Server closed."); }
 			public void disconnected(){ console.println(ChatColor.RED+"Disconnected from server."); }
 			public void couldNotConnect(){ console.println(ChatColor.RED+"Could not connect to server."); }
 			public void connectedToServer(){ console.println(ChatColor.GREEN+"Connected to server."); }
 		});
-		if(client.isConnected())client.send("000"+new HandshakePacket().compress());
+		if(client.isConnected())client.send("001"+new HandshakePacket().compress());
 	}
 }
