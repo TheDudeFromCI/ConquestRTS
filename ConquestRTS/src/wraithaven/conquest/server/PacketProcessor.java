fd7629f99a9b58f19f6be1d9673fb3110772ce57
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-13 03:55:53 -0700

    Moved packet processor to seperate class.

---
+++ ConquestRTS/src/wraithaven/conquest/server/PacketProcessor.java
@@ -0,0 +1,36 @@
+package wraithaven.conquest.server;
+
+import java.io.PrintWriter;
+import wraithaven.conquest.HandshakePacket;
+import wraithaven.conquest.Packet;
+import wraithaven.conquest.PacketType;
+import wraith.library.Multiplayer.ClientInstance;
+import wraith.library.Multiplayer.ServerListener;
+
+public class PacketProcessor implements ServerListener{
+	public void recivedInput(ClientInstance client, String s){
+		Packet packet = PacketType.create(s);
+		Player player = ServerLauncher.channelManager.getPlayer(client);
+		if(packet==null){
+			player.kick("Sent unknown packet.");
+			return;
+		}
+		if(packet.getPacketType()==PacketType.handshake){
+			if(((HandshakePacket)packet).isCorrectFormat()){
+				if(player.awaitingHandshake())player.shakeHand();
+				else player.kick("Tried to handshake twice.");
+			}else player.kick("Wrong handshake format.");
+			return;
+		}else{
+			if(player.awaitingHandshake()){
+				player.kick("Failed to handshake.");
+				return;
+			}
+			//TODO Process other packets.
+		}
+		ServerLauncher.channelManager.getLobby().sendChannelPacket(packet);
+	}
+	public void serverClosed(){ System.exit(0); }
+	public void clientDisconnected(ClientInstance client){ ServerLauncher.channelManager.removePlayer(ServerLauncher.channelManager.getPlayer(client)); }
+	public void clientConncted(ClientInstance client, PrintWriter out){ ServerLauncher.channelManager.addPlayer(new Player(client, out)); }
+}
