df457b961d1dfc3e78bab9f1931a1c352d2d70f8
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-13 17:29:01 -0700

    Removed multiplayer component, and started on actual game window.

--- ConquestRTS/src/wraithaven/conquest/client/ClientLauncher.java
+++ ConquestRTS/src/wraithaven/conquest/client/ClientLauncher.java
@@ -1,45 +1,12 @@
 package wraithaven.conquest.client;
 
-import wraithaven.conquest.ChatMessagePacket;
-import wraithaven.conquest.KickMessagePacket;
-import wraithaven.conquest.Packet;
-import wraithaven.conquest.PacketType;
-import wraithaven.conquest.HandshakePacket;
-import wraith.library.WindowUtil.ChatColor;
-import wraith.library.Multiplayer.ClientListener;
-import wraith.library.Multiplayer.Client;
-import wraith.library.WindowUtil.ConsoleListener;
-import wraith.library.WindowUtil.ColorConsole;
+import wraith.library.WindowUtil.GameScreen;
 
 public class ClientLauncher{
-	private static Client client;
-	private static ColorConsole console;
 	public static void main(String[] args){
-		console=new ColorConsole(true);
-		console.setTitle("Wraithaven's Conquest");
-		console.addConsoleListener(new ConsoleListener(){
-			public void onCommandSent(String msg){ client.send("000Player¤"+msg); }
-		});
-		client=new Client("localhost", 10050, new ClientListener(){
-			public void recivedInput(String s){
-				Packet packet = PacketType.create(s);
-				if(packet==null)return;
-				if(packet instanceof KickMessagePacket){
-					console.println(ChatColor.ORANGE+((KickMessagePacket)packet).getMessage());
-					return;
-				}
-				if(packet instanceof ChatMessagePacket){
-					ChatMessagePacket chatPacket = (ChatMessagePacket)packet;
-					console.println(ChatColor.YELLOW+chatPacket.getSender()+": "+chatPacket.getMessage());
-					return;
-				}
-			}
-			public void unknownHost(){ console.println(ChatColor.RED+"Unknown host."); }
-			public void serverClosed(){ console.println(ChatColor.RED+"Server closed."); }
-			public void disconnected(){ console.println(ChatColor.RED+"Disconnected from server."); }
-			public void couldNotConnect(){ console.println(ChatColor.RED+"Could not connect to server."); }
-			public void connectedToServer(){ console.println(ChatColor.GREEN+"Connected to server."); }
-		});
-		if(client.isConnected())client.send("001"+new HandshakePacket().compress());
+		RenderingPanel renderingPanel = new RenderingPanel();
+		GameScreen game = new GameScreen("Wraithaven's Conquest", renderingPanel, renderingPanel);
+		game.setRenderSize(4, 3, true);
+		renderingPanel.setDimensions(game.getRenderSize().width, game.getRenderSize().height);
 	}
 }
