21220dc60cf68087336cc86d572fc343ea63aadc
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-17 20:51:47 -0700

    Moved tile renderer out of the way, preparing for the title screen.

---
+++ ConquestRTS/src/wraithaven/conquest/client/TitleScreen.java
@@ -0,0 +1,10 @@
+package wraithaven.conquest.client;
+
+import java.awt.Graphics2D;
+import wraith.library.WindowUtil.GameRenderer;
+import wraith.library.WindowUtil.UserInputAdapter;
+
+public class TitleScreen extends UserInputAdapter implements GameRenderer{
+	public void render(Graphics2D arg0, int arg1, int arg2, int arg3, int arg4){
+	}
+}
