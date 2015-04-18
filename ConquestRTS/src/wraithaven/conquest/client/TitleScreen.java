562cc659f6fb0a0584b3ead48e9f8116e85ccbf1
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-17 22:08:52 -0700

    Switched to using WraithEngine game utility.

--- ConquestRTS/src/wraithaven/conquest/client/TitleScreen.java
+++ ConquestRTS/src/wraithaven/conquest/client/TitleScreen.java
@@ -2,8 +2,8 @@ package wraithaven.conquest.client;
 
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
-import wraith.library.WindowUtil.GameRenderer;
 import wraith.library.WindowUtil.UserInputAdapter;
+import wraith.library.WindowUtil.GameWindow.GameRenderer;
 
 public class TitleScreen extends UserInputAdapter implements GameRenderer{
 	public void render(Graphics2D g, int x, int y, int width, int height){
