34ef70abfa711a0dedddad826791fab7e23fb7bc
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-20 01:02:58 -0700

    Synced to WraithEngine's new gui adjustments.

--- ConquestRTS/src/wraithaven/conquest/client/ClientLauncher.java
+++ ConquestRTS/src/wraithaven/conquest/client/ClientLauncher.java
@@ -2,10 +2,19 @@ package wraithaven.conquest.client;
 
 import java.io.File;
 import wraith.library.WindowUtil.GameWindow.Game;
+import wraith.library.WindowUtil.GameWindow.PendingTask;
 
 public class ClientLauncher{
 	public static void main(String[] args){
-		Game game = new Game("Wraithaven's Conquest", new File("C:/Users/Phealoon/Desktop/Conquest Folder"));
+		final Game game = new Game("Wraithaven's Conquest", new File("C:/Users/Phealoon/Desktop/Conquest Folder"));
 		game.getScreen().setRenderSize(4, 3, true);
+		new PendingTask(game.getGameThread(), 0){
+			public void run(){
+				TitleScreen titleScreen = new TitleScreen(game);
+				game.setGameRenderer(titleScreen);
+				game.setInputListener(titleScreen.getGui());
+			}
+			public int getPriority(){ return 100; }
+		};
 	}
 }
--- ConquestRTS/src/wraithaven/conquest/client/TitleScreen.java
+++ ConquestRTS/src/wraithaven/conquest/client/TitleScreen.java
@@ -1,12 +1,20 @@
 package wraithaven.conquest.client;
 
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
-import wraith.library.WindowUtil.UserInputAdapter;
+import java.awt.image.BufferedImage;
+import wraith.library.WindowUtil.GUI.GuiFrame;
+import wraith.library.WindowUtil.GameWindow.Game;
 import wraith.library.WindowUtil.GameWindow.GameRenderer;
 
-public class TitleScreen extends UserInputAdapter implements GameRenderer{
-	public void render(Graphics2D g, int x, int y, int width, int height){
+public class TitleScreen implements GameRenderer{
+	private Game game;
+	private BufferedImage bg;
+	private GuiFrame gui;
+	public TitleScreen(Game game){
+		this.game=game;
+		bg=this.game.getGameDataFolder().getImage("Title Screen.png");
+		gui=new GuiFrame(game.getScreenWidth(), game.getScreenHeight());
 	}
-	@Override public void keyPressed(KeyEvent e){ if(e.getKeyCode()==KeyEvent.VK_F12)System.exit(0); }
+	public void render(Graphics2D g, int x, int y, int width, int height){ g.drawImage(gui.getPane(), x, y, null); }
+	public GuiFrame getGui(){ return gui; }
 }
