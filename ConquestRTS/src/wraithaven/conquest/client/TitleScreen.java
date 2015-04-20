58f32f57cd881941258e5dfe7bde4895cda62bfb
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-20 12:02:54 -0700

    Added exit button.

--- ConquestRTS/src/wraithaven/conquest/client/TitleScreen.java
+++ ConquestRTS/src/wraithaven/conquest/client/TitleScreen.java
@@ -9,16 +9,27 @@ import wraith.library.WindowUtil.GameWindow.GameRenderer;
 
 public class TitleScreen implements GameRenderer{
 	private GuiFrame gui;
-	public TitleScreen(Game game){
+	private GuiButton multiplayerButton;
+	private GuiButton exitButton;
+	public TitleScreen(final Game game){
 		gui=new GuiFrame(game.getScreenWidth(), game.getScreenHeight());
 		gui.getOffset().x=game.getRenderX();
 		gui.getOffset().y=game.getRenderY();
 		gui.setLayout(new TitleScreenLayout());
-		GuiButton button = new GuiButton(gui, game.getGameDataFolder().getImage("Multiplayer Button.png"), true, new Runnable(){
+		multiplayerButton=new GuiButton(gui, game.getGameDataFolder().getImage("Multiplayer Button.png"), true, new Runnable(){
+			public void run(){
+				game.getScreen().removeUserInputListener(multiplayerButton);
+				game.getScreen().removeUserInputListener(exitButton);
+				gui.dispose();
+				game.setGameRenderer(new ServerList(game));
+			}
+		});
+		exitButton=new GuiButton(gui, game.getGameDataFolder().getImage("Exit Button.png"), true, new Runnable(){
 			public void run(){ System.exit(0); }
 		});
-		game.getScreen().addUserInputListener(button);
-		gui.addComponent(new GuiImage(gui, game.getGameDataFolder().getImage("Title Screen.png"), true), button);
+		game.getScreen().addUserInputListener(multiplayerButton);
+		game.getScreen().addUserInputListener(exitButton);
+		gui.addComponent(new GuiImage(gui, game.getGameDataFolder().getImage("Title Screen.png"), true), multiplayerButton, exitButton);
 		gui.update();
 	}
 	public void render(Graphics2D g, int x, int y, int width, int height){ g.drawImage(gui.getPane(), x, y, null); }
--- ConquestRTS/src/wraithaven/conquest/client/TitleScreenLayout.java
+++ ConquestRTS/src/wraithaven/conquest/client/TitleScreenLayout.java
@@ -8,6 +8,7 @@ public class TitleScreenLayout implements GuiLayout{
 	private int tempX, tempY, tempWidth, tempHeight;
 	private static final int BUTTON_HEIGHT = 100;
 	private static final int BUTTON_WIDTH = 300;
+	private static final int BUTTON_SEPERATION = 20;
 	public void setParentDimensions(int x, int y, int width, int height){
 		tempX=x;
 		tempY=y;
@@ -15,7 +16,11 @@ public class TitleScreenLayout implements GuiLayout{
 		tempHeight=height;
 	}
 	public void validateComponents(ArrayList<GuiComponent> components){
-		if(components.size()>=1)components.get(0).setSizeAndLocation(tempX, tempY, tempWidth, tempHeight);
-		if(components.size()>=2)components.get(1).setSizeAndLocation(tempX+(tempWidth-BUTTON_WIDTH)/2, tempY+(tempHeight-BUTTON_HEIGHT)/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+		if(components.isEmpty())return;
+		int startY = tempY+(tempHeight-(BUTTON_HEIGHT*2+BUTTON_SEPERATION))/2;
+		int nextY = BUTTON_HEIGHT+BUTTON_SEPERATION;
+		components.get(0).setSizeAndLocation(tempX, tempY, tempWidth, tempHeight);
+		components.get(1).setSizeAndLocation(tempX+(tempWidth-BUTTON_WIDTH)/2, startY, BUTTON_WIDTH, BUTTON_HEIGHT);
+		components.get(2).setSizeAndLocation(tempX+(tempWidth-BUTTON_WIDTH)/2, startY+nextY, BUTTON_WIDTH, BUTTON_HEIGHT);
 	}
 }
