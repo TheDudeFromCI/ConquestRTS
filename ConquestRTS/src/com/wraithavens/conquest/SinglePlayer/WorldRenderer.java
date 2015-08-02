489f5663f48a002d6839d563d843690699cf2f62
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-08-02 11:01:02 -0700

    Deleted many old trash world renderers.

--- ConquestRTS/src/com/wraithavens/conquest/SinglePlayer/WorldRenderer.java
+++
@@ -1,9 +0,0 @@
-package com.wraithavens.conquest.SinglePlayer;
-
-public class WorldRenderer{
-	private static final int SIZE = 16;
-	private final ChunkMesh[] chunkMeshes = new ChunkMesh[SIZE*SIZE*SIZE];
-	public WorldRenderer(){
-
-	}
-}
