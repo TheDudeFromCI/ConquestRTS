5665b2353919d00eeb85c0b20edbf5906a04756f
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-08-03 03:33:00 -0700

    Increased heightmap generation speed.

--- ConquestRTS/src/com/wraithavens/conquest/SinglePlayer/Heightmap/HeightmapGenerator.java
+++ ConquestRTS/src/com/wraithavens/conquest/SinglePlayer/Heightmap/HeightmapGenerator.java
@@ -5,34 +5,15 @@ import java.nio.FloatBuffer;
 import com.wraithavens.conquest.Launcher.WraithavensConquest;
 import com.wraithavens.conquest.Math.Vector3f;
 import com.wraithavens.conquest.SinglePlayer.Noise.WorldNoiseMachine;
-import com.wraithavens.conquest.Utility.CompactBinaryFile;
+import com.wraithavens.conquest.Utility.BinaryFile;
 
 public class HeightmapGenerator{
-	private static void loadHeightmap(FloatBuffer vertexData, String file){
+	private static void loadHeightmap(FloatBuffer vertexData, File file){
 		System.out.println("Loading heightmap.");
-		CompactBinaryFile f = new CompactBinaryFile(file);
-		f.read();
-		for(int i = 0; i<HeightMap.Vertices; i++){
-			// ---
-			// Load height.
-			// ---
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			// ---
-			// Load color.
-			// ---
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			// ---
-			// Load normal.
-			// ---
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-			vertexData.put(Float.intBitsToFloat((int)f.getNumber(32)));
-		}
-		f.stopReading();
+		BinaryFile bin = new BinaryFile(file);
+		int floats = HeightMap.Vertices*9;
+		for(int i = 0; i<floats; i++)
+			vertexData.put(bin.getFloat());
 		System.out.println("Heightmap loaded.");
 	}
 	private final WorldNoiseMachine machine;
@@ -52,7 +33,7 @@ public class HeightmapGenerator{
 			+z7-z0-2.0f*z1-z2));
 		out.normalize();
 	}
-	private void generateHeightmap(int x, int z, FloatBuffer vertexData, String file){
+	private void generateHeightmap(int x, int z, FloatBuffer vertexData, File file){
 		System.out.println("Generating heightmap: "+x+", "+z+"");
 		int a, b;
 		float s = HeightMap.ViewDistance*2/(HeightMap.VertexCount-1.0f);
@@ -61,10 +42,7 @@ public class HeightmapGenerator{
 		float height;
 		Vector3f normal = new Vector3f();
 		Vector3f color = new Vector3f();
-		CompactBinaryFile f = new CompactBinaryFile(file);
-		f.ensureExistance();
-		f.write();
-		f.prepareSpace(HeightMap.Vertices*36);
+		BinaryFile bin = new BinaryFile(HeightMap.Vertices*36);
 		for(b = 0; b<HeightMap.VertexCount; b++)
 			for(a = 0; a<HeightMap.VertexCount; a++){
 				// ---
@@ -80,29 +58,29 @@ public class HeightmapGenerator{
 				vertexData.put(blockX);
 				vertexData.put(height = (float)machine.getWorldHeight(blockX, blockZ));
 				vertexData.put(blockZ);
-				f.addNumber(Float.floatToIntBits(blockX), 32);
-				f.addNumber(Float.floatToIntBits(height), 32);
-				f.addNumber(Float.floatToIntBits(blockZ), 32);
+				bin.addFloat(blockX);
+				bin.addFloat(height);
+				bin.addFloat(blockZ);
 				// ---
 				// Bind the vertex color.
 				// ---
 				vertexData.put(color.x);
 				vertexData.put(color.y);
 				vertexData.put(color.z);
-				f.addNumber(Float.floatToIntBits(color.x), 32);
-				f.addNumber(Float.floatToIntBits(color.y), 32);
-				f.addNumber(Float.floatToIntBits(color.z), 32);
+				bin.addFloat(color.x);
+				bin.addFloat(color.y);
+				bin.addFloat(color.z);
 				// ---
 				// Bind the vertex normal.
 				// ---
 				vertexData.put(normal.x);
 				vertexData.put(normal.y);
 				vertexData.put(normal.z);
-				f.addNumber(Float.floatToIntBits(normal.x), 32);
-				f.addNumber(Float.floatToIntBits(normal.y), 32);
-				f.addNumber(Float.floatToIntBits(normal.z), 32);
+				bin.addFloat(normal.x);
+				bin.addFloat(normal.y);
+				bin.addFloat(normal.z);
 			}
-		f.stopWriting();
+		bin.compile(file);
 		System.out.println("Heightmap generated.");
 	}
 	void getHeightmap(int x, int z, FloatBuffer vertexData){
@@ -112,8 +90,8 @@ public class HeightmapGenerator{
 		// Otherwise generate a new one, and save it.
 		// ---
 		if(!file.exists()||file.length()==0)
-			generateHeightmap(x, z, vertexData, file.getAbsolutePath());
+			generateHeightmap(x, z, vertexData, file);
 		else
-			loadHeightmap(vertexData, file.getAbsolutePath());
+			loadHeightmap(vertexData, file);
 	}
 }
---
+++ ConquestRTS/src/com/wraithavens/conquest/Utility/BinaryFile.java
@@ -0,0 +1,108 @@
+package com.wraithavens.conquest.Utility;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class BinaryFile{
+	private static byte[] read(File file){
+		BufferedInputStream in = null;
+		try{
+			in = new BufferedInputStream(new FileInputStream(file));
+			byte[] d = new byte[in.available()];
+			in.read(d);
+			return d;
+		}catch(Exception exception){
+			exception.printStackTrace();
+		}finally{
+			if(in!=null)
+				try{
+					in.close();
+				}catch(Exception exception){
+					exception.printStackTrace();
+				}
+		}
+		return null;
+	}
+	private static void write(File file, byte[] binary){
+		BufferedOutputStream out = null;
+		try{
+			out = new BufferedOutputStream(new FileOutputStream(file));
+			out.write(binary);
+		}catch(Exception exception){
+			exception.printStackTrace();
+		}finally{
+			if(out!=null)
+				try{
+					out.close();
+				}catch(Exception exception){
+					exception.printStackTrace();
+				}
+		}
+	}
+	private byte[] binary;
+	private int pos;
+	/**
+	 * Reads all binary data from a file into a byte array.
+	 */
+	public BinaryFile(File file){
+		binary = read(file);
+	}
+	/**
+	 * Creates an empty byte array of the desired size.
+	 */
+	public BinaryFile(int space){
+		binary = new byte[space];
+	}
+	public void addFloat(float n){
+		addInt(Float.floatToIntBits(n));
+	}
+	public void addInt(int n){
+		binary[pos] = (byte)(n&0xFF);
+		binary[pos+1] = (byte)(n>>8&0xFF);
+		binary[pos+2] = (byte)(n>>16&0xFF);
+		binary[pos+3] = (byte)(n>>24&0xFF);
+		pos += 4;
+	}
+	public void addShort(short n){
+		binary[pos] = (byte)(n&0xFF);
+		binary[pos+1] = (byte)(n>>8&0xFF);
+		pos += 2;
+	}
+	public void compile(File file){
+		if(!file.exists()){
+			try{
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+			}catch(Exception exception){
+				exception.printStackTrace();
+				return;
+			}
+		}
+		write(file, binary);
+	}
+	public byte getByte(){
+		byte b = binary[pos];
+		pos++;
+		return b;
+	}
+	public float getFloat(){
+		return Float.intBitsToFloat(getInt());
+	}
+	public int getInt(){
+		int i = binary[pos]+(binary[pos+1]<<8)+(binary[pos+2]<<16)+(binary[pos+3]<<24);
+		pos += 4;
+		return i;
+	}
+	public short getShort(){
+		short s = (short)(binary[pos]+(binary[pos+1]<<8));
+		pos += 2;
+		return s;
+	}
+	public void placeByte(byte n){
+		binary[pos] = n;
+		pos++;
+	}
+}
