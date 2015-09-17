e0a1c38920a4dd98954ca04e8c81eaf3553ae4fc
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-09-17 08:45:10 -0700

    Updating point generation protocol a little.
    Signed-off-by: TheDudeFromCI <linktheblademaster@yahoo.com>

--- ConquestRTS/src/com/wraithavens/conquest/SinglePlayer/Noise/PointGenerator2D.java
+++ ConquestRTS/src/com/wraithavens/conquest/SinglePlayer/Noise/PointGenerator2D.java
@@ -17,35 +17,36 @@ public class PointGenerator2D{
 		this.seed = seed;
 		this.averageDistanceApart = averageDistanceApart;
 		this.consistancy = consistancy;
-		maxShift = minDistanceApart==0?0:minDistanceApart/averageDistanceApart;
+		maxShift = (minDistanceApart==0?0:minDistanceApart/averageDistanceApart)*0.5f;
 	}
 	public void noise(float x, float y, float size, ArrayList<float[]> points){
-		int startX = floor(x/averageDistanceApart)-1;
-		int startY = floor(y/averageDistanceApart)-1;
-		int endX = floor(x/averageDistanceApart+size/averageDistanceApart)+1;
-		int endY = floor(y/averageDistanceApart+size/averageDistanceApart)+1;
+		float subX = x/averageDistanceApart;
+		float subY = y/averageDistanceApart;
+		float subSize = size/averageDistanceApart;
+		int startX = floor(subX)-1;
+		int startY = floor(subY)-1;
+		int endX = floor(subX+subSize)+1;
+		int endY = floor(subY+subSize)+1;
 		int a, b;
 		float tempX, tempZ;
 		for(a = startX; a<=endX; a++)
 			for(b = startY; b<=endY; b++){
-				if(random(a, b, 0)>=consistancy)
+				if(random(a, b)>=consistancy)
 					continue;
-				tempX = (a+(random(a, b, 1)*2-1)*maxShift)*averageDistanceApart;
-				tempZ = (b+(random(a, b, 2)*2-1)*maxShift)*averageDistanceApart;
+				tempX = (a+(r.nextFloat()*2-1)*maxShift)*averageDistanceApart;
+				tempZ = (b+(r.nextFloat()*2-1)*maxShift)*averageDistanceApart;
 				if(tempX>=x&&tempZ>=y&&tempX<x+size&&tempZ<y+size)
 					points.add(new float[]{
-						tempX, tempZ, (float)(random(a, b, 3)*Math.PI*2), random(a, b, 4)*0.2f+0.9f
+						tempX, tempZ, (float)(r.nextFloat()*Math.PI*2), r.nextFloat()*0.2f+0.9f
 					});
 			}
 	}
-	private float random(int x, int y, int z){
+	private float random(int x, int y){
 		long t = seed;
 		t = t*s+x;
 		t = t*s+y;
-		t = t*s+z;
 		t += x*x+s;
 		t += y*y+s;
-		t += z*z+s;
 		r.setSeed(t);
 		return r.nextFloat();
 	}
