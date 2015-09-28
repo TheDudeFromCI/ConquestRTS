9ca7e9c7c6325d3f4565824b38b5f3639764f347
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-09-27 18:41:26 -0700

    Updated Aesia pedal particle colors.
    Signed-off-by: TheDudeFromCI <linktheblademaster@yahoo.com>

--- ConquestRTS/src/com/wraithavens/conquest/SinglePlayer/Particles/ParticleTypes/AesiaStemPedal.java
+++ ConquestRTS/src/com/wraithavens/conquest/SinglePlayer/Particles/ParticleTypes/AesiaStemPedal.java
@@ -13,7 +13,29 @@ public class AesiaStemPedal extends Particle{
 		endY = y-6;
 		location.set(x, y, z);
 		scale.set(1/7f, 1/7f);
-		color.set(200/255f, 140/255f, 120/255f, 1);
+		int randomColor = (int)(Math.random()*6);
+		switch(randomColor){
+			case 0:
+				color.set(249/255f, 181/255f, 223/255f, 1);
+				break;
+			case 1:
+				color.set(231/255f, 143/255f, 197/255f, 1);
+				break;
+			case 2:
+				color.set(152/255f, 82/255f, 125/255f, 1);
+				break;
+			case 3:
+				color.set(112/255f, 29/255f, 81/255f, 1);
+				break;
+			case 4:
+				color.set(200/255f, 118/255f, 169/255f, 1);
+				break;
+			case 5:
+				color.set(234/255f, 114/255f, 188/255f, 1);
+				break;
+			default:
+				throw new AssertionError();
+		}
 		randomX = (float)(Math.random()*2-1);
 		randomZ = (float)(Math.random()*2-1);
 	}
