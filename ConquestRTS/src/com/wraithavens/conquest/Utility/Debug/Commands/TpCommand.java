3361b39a36aa4aa9eac08f82d73e230fc9335717
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-10-05 20:49:50 -0700

    Added teleport to block command.
    Signed-off-by: TheDudeFromCI <linktheblademaster@yahoo.com>

--- ConquestRTS/src/com/wraithavens/conquest/Utility/Debug/Commands/TpCommand.java
+++ ConquestRTS/src/com/wraithavens/conquest/Utility/Debug/Commands/TpCommand.java
@@ -25,60 +25,93 @@ public class TpCommand implements Command{
 		return "tp [biome] {-maxRange:#}";
 	}
 	public void parse(String[] args){
-		if(args.length<2){
+		if(args.length!=3&&args.length!=4){
 			console.println(ChatColor.RED+"Error! Unknown number of arguments.");
 			return;
 		}
-		int maxRangeFirstPass = Integer.MAX_VALUE;
-		for(String s : args){
-			if(s==args[0]||s==args[1])
-				continue;
-			if(s.startsWith("-maxRange")){
-				try{
-					maxRangeFirstPass = Integer.valueOf(s.substring(9));
-				}catch(Exception exception){
-					console.println(ChatColor.RED+"That's not a number! '"+s.substring(9)+"'");
-					return;
-				}
-			}else
-				console.println(ChatColor.YELLOW+"Warning! Unknown argument: '"+s+"'.");
-		}
-		final int maxRange = maxRangeFirstPass;
-		final Biome targetBiome = Biome.getByName(args[1]);
-		if(targetBiome==null){
-			console.println(ChatColor.RED+"Error! Biome not found. '"+args[1]+"'.");
-			return;
-		}
-		console.println(ChatColor.GREEN+"Begining search...");
-		MainLoop.endLoopTasks.add(new Runnable(){
-			public void run(){
-				SinglePlayerGame game = SinglePlayerGame.INSTANCE;
-				if(game==null){
-					console.println(ChatColor.RED+"Error! Game not initalized!");
-					return;
-				}
-				WorldNoiseMachine machine = game.getWorldNoiseMachine();
-				SpiralGridAlgorithm grid = new SpiralGridAlgorithm();
-				int searchSize = 100;
-				grid.setMaxDistance(maxRange/searchSize);
-				Camera camera = game.getCamera();
-				int x, z;
-				float[] temp = new float[3];
-				while(true){
-					x = grid.getX()*searchSize+(int)camera.x;
-					z = grid.getY()*searchSize+(int)camera.z;
-					if(machine.getBiomeAt(x, z, temp)==targetBiome){
-						camera.teleport(x, machine.getGroundLevel(x, z)+6, z);
-						break;
-					}
-					if(!grid.hasNext()){
-						console.println(ChatColor.YELLOW+"Biome not found.");
+		if(args.length==2){
+			int maxRangeFirstPass = Integer.MAX_VALUE;
+			for(String s : args){
+				if(s==args[0]||s==args[1])
+					continue;
+				if(s.startsWith("-maxRange")){
+					try{
+						maxRangeFirstPass = Integer.valueOf(s.substring(9));
+					}catch(Exception exception){
+						console.println(ChatColor.RED+"That's not a number! '"+s.substring(9)+"'");
 						return;
 					}
-					grid.next();
-				}
-				console.println(ChatColor.GREEN+"Biome found.");
+				}else
+					console.println(ChatColor.YELLOW+"Warning! Unknown argument: '"+s+"'.");
 			}
-		});
+			final int maxRange = maxRangeFirstPass;
+			final Biome targetBiome = Biome.getByName(args[1]);
+			if(targetBiome==null){
+				console.println(ChatColor.RED+"Error! Biome not found. '"+args[1]+"'.");
+				return;
+			}
+			console.println(ChatColor.GREEN+"Begining search...");
+			MainLoop.endLoopTasks.add(new Runnable(){
+				public void run(){
+					SinglePlayerGame game = SinglePlayerGame.INSTANCE;
+					if(game==null){
+						console.println(ChatColor.RED+"Error! Game not initalized!");
+						return;
+					}
+					WorldNoiseMachine machine = game.getWorldNoiseMachine();
+					SpiralGridAlgorithm grid = new SpiralGridAlgorithm();
+					int searchSize = 100;
+					grid.setMaxDistance(maxRange/searchSize);
+					Camera camera = game.getCamera();
+					int x, z;
+					float[] temp = new float[3];
+					while(true){
+						x = grid.getX()*searchSize+(int)camera.x;
+						z = grid.getY()*searchSize+(int)camera.z;
+						if(machine.getBiomeAt(x, z, temp)==targetBiome){
+							camera.teleport(x, machine.getGroundLevel(x, z)+6, z);
+							break;
+						}
+						if(!grid.hasNext()){
+							console.println(ChatColor.YELLOW+"Biome not found.");
+							return;
+						}
+						grid.next();
+					}
+					console.println(ChatColor.GREEN+"Biome found.");
+				}
+			});
+		}else{
+			float x, y, z;
+			try{
+				x = Float.valueOf(args[1]);
+			}catch(NumberFormatException exception){
+				console.println(ChatColor.RED+"Thats not a number! '"+args[1]+"'");
+				return;
+			}
+			try{
+				y = Float.valueOf(args[2]);
+			}catch(NumberFormatException exception){
+				console.println(ChatColor.RED+"Thats not a number! '"+args[2]+"'");
+				return;
+			}
+			try{
+				z = Float.valueOf(args[3]);
+			}catch(NumberFormatException exception){
+				console.println(ChatColor.RED+"Thats not a number! '"+args[3]+"'");
+				return;
+			}
+			MainLoop.endLoopTasks.add(new Runnable(){
+				public void run(){
+					SinglePlayerGame game = SinglePlayerGame.INSTANCE;
+					if(game==null){
+						console.println(ChatColor.RED+"Error! Game not initalized!");
+						return;
+					}
+					game.getCamera().teleport(x, y, z);
+					console.println(ChatColor.GREEN+"Teleported to ("+x+", "+y+", "+z+").");
+				}
+			});
+		}
 	}
 }
